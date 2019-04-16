074e7bb65ceb2fe81e990e3e263d95287a10299f
Laurabelle <laurabellek@yahoo.com>
2019-04-15 20:20:40 -0700

    updated to use graph

--- display.cpp
+++ display.cpp
@@ -8,7 +8,6 @@ void drawGrid(int gridx, int gridy, Point **points, Wire *W)
     sf::RenderWindow window(sf::VideoMode(800, 600), "Grid");
     vector<vector<sf::RectangleShape>> grid(gridx);
 
-
     while (window.isOpen())
     {
         sf::Event event;
@@ -35,6 +34,50 @@ void drawGrid(int gridx, int gridy, Point **points, Wire *W)
                 else if(points[i][j].obstructedBy != -2)
                 {
                     int ind = points[i][j].obstructedBy;
+                    grid[i][j].setFillColor(sf::Color(W[ind].r, W[ind].g, W[ind].b));
+                }
+                grid[i][j].setOutlineColor(sf::Color::Black);
+                grid[i][j].setOutlineThickness(1.0f);
+
+                grid[i][j].setPosition(i * cellSize.x + 5.0f, j * cellSize.y + 5.0f);
+
+                window.draw(grid[i][j]);
+            }
+        }
+        window.display();
+    }
+}
+
+void drawGrid(int gridx, int gridy, int *graph, Wire *W)
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Grid");
+    vector<vector<sf::RectangleShape>> grid(gridx);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        sf::Vector2f cellSize((float)800.0 / gridx, (float)600.0 / gridy);
+
+        for (int i = 0; i < gridx; i++)
+        {
+            grid[i] = vector<sf::RectangleShape>(gridy);
+            for (int j = 0; j < gridy; j++)
+            {
+                grid[i][j].setSize(cellSize);
+                grid[i][j].setFillColor(sf::Color::White);
+                if (graph[gridy * i + j] == -1)
+                {
+                    grid[i][j].setFillColor(sf::Color::Black);
+                }
+                else if (graph[gridy * i + j] != -2)
+                {
+                    int ind = graph[gridy * i + j];
                     grid[i][j].setFillColor(sf::Color(W[ind].r, W[ind].g, W[ind].b));
                 }
                 grid[i][j].setOutlineColor(sf::Color::Black);
